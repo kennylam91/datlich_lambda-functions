9bac4d76acc029d980bb82453c46d5b8f4893a70
lampv <lam.phamvan@nttdata.com>
2024-10-24 17:59:58 +0700

    adapt job and tests

--- service-providers/main_test.go
+++ service-providers/main_test.go
@@ -10,7 +10,8 @@ func Test_buildPutItem(t *testing.T) {
 	type args struct {
 		eventBody string
 	}
-	tests := []struct {
+	testPassword := "pass"
+	var tests = []struct {
 		name    string
 		args    args
 		want    map[string]types.AttributeValue
@@ -18,12 +19,11 @@ func Test_buildPutItem(t *testing.T) {
 	}{
 		{
 			name: "valid body",
-			args: args{eventBody: `{"name":"doctor", "email": "test@example.com", "phone": "0123456", "password": "pass"}`},
+			args: args{eventBody: `{"name":"doctor", "email": "test@example.com", "phone": "0123456", "password": "` + testPassword + `"}`},
 			want: map[string]types.AttributeValue{
-				"name":     &types.AttributeValueMemberS{Value: "doctor"},
-				"email":    &types.AttributeValueMemberS{Value: "test@example.com"},
-				"phone":    &types.AttributeValueMemberS{Value: "0123456"},
-				"password": &types.AttributeValueMemberS{Value: "pass"},
+				"name":  &types.AttributeValueMemberS{Value: "doctor"},
+				"email": &types.AttributeValueMemberS{Value: "test@example.com"},
+				"phone": &types.AttributeValueMemberS{Value: "0123456"},
 			},
 			wantErr: false,
 		},
@@ -35,7 +35,7 @@ func Test_buildPutItem(t *testing.T) {
 				t.Errorf("buildPutItem() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			for _, field := range []string{"name", "email", "phone", "password"} {
+			for _, field := range []string{"name", "email", "phone"} {
 				if !reflect.DeepEqual(got[field], tt.want[field]) {
 					t.Errorf("buildPutItem() field '%v' got = %v, want %v", field, got[field], tt.want[field])
 					return
