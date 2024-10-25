2bb31c80d19709bc5f9806a1851edcb1abc12824
lampv <lam.phamvan@nttdata.com>
2024-10-25 13:58:55 +0700

    commit

---
+++ service-provider-login/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_getBasicCredential(t *testing.T) {
+	type args struct {
+		body string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    BasicCredential
+		wantErr bool
+	}{
+		{
+			name:    "case ok",
+			args:    args{body: `{"email": "test@example.com", "password": "pass123@"}`},
+			want:    BasicCredential{"test@example.com", "pass123@"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getBasicCredential(tt.args.body)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getBasicCredential() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getBasicCredential() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
