d6766f4648e63dbb7fc0376d30f36f1658d5a6f6
lampv <lam.phamvan@nttdata.com>
2024-10-25 14:04:15 +0700

    set Expires for cookie

--- service-provider-login/main.go
+++ service-provider-login/main.go
@@ -14,6 +14,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"time"
 )
 
 var emailIndex = "email-index"
@@ -83,6 +84,7 @@ func buildSessionCookie(provider ServiceProvider) *http.Cookie {
 		Path:     "/",
 		HttpOnly: true,
 		SameSite: http.SameSiteLaxMode,
+		Expires:  time.Now().Add(8 * time.Hour),
 	}
 }
 
